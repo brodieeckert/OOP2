724d671c926b9097e9617bb293c73659dc01a657
Brodie <brodieeckert29@gmail.com>
2022-08-16 11:12:46 +0930

    workshop4 edits

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -2,7 +2,11 @@
 // use a function pointer to call the appropriate operator function
 // eg to add       arithmetic_ops(10,2,add_op)
 // eg to subtract  arithmetic_ops(10,2,subtract_op)
+float add_op(float left, float right){return (left + right);}
+float subtract_op(float left, float right){return (left - right);}
 float multiply_op(float left, float right){return (left * right);}
+float divide_op(float left, float right){return (left / right);}
+
 
 float arithmetic_ops(float left, float right, float (*op)(float,float))
 {
