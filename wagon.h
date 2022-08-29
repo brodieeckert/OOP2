b8192eb57f29320e4f068d48f2d0cdbd6c3947a4
Brodie <brodieeckert29@gmail.com>
2022-08-29 14:49:29 +0930

    yeet

--- wagon.h
+++ wagon.h
@@ -1,34 +1,19 @@
 #include <iostream>
-#include "Wagon.h"
-
+#include <string>
+#include "Capybara.h"
 using namespace std;
 
-Wagon::Wagon(){
+class Wagon: public Capybara{
+    protected:
+        int number = 0;
 
-};
+        Capybara seat[4];
 
-bool Wagon::addCapybara(Capybara newCapy){
-    number++;
-    cout << number << endl;
-    if(number >=4){
-        cout << " your an idiot \n";
-        return false;
-        }
-        else{    
-            seat[number] = newCapy;
-        }
-    
+    public:
+        Wagon();
+        bool addCapybara(Capybara newCapy);
+        void emptyWagon();
+        void printCapybaras();
 
-    return true;
-}
-void Wagon::emptyWagon(){
-    for(int i =0;i<4;i++){
-        this->seat[i] = Capybara();
-    }
-    this->number = 0;
-}
-void Wagon::printCapybaras(){
-    for(int i = 0; i<number;i++){
-        cout << "Name and age: " << seat[i].getName() << " " << seat[i].getAge() << endl;
-    }
-}
+
+}; 
