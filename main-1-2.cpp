07cc2356280e585497e2fe4afaa5e0a8d9532540
Brodie <brodieeckert29@gmail.com>
2022-09-06 11:19:27 +0930

    7

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -18,6 +18,13 @@ int main(){
     cout << O.get_current_number_of_members() << endl;
     O.add_musician(m4);
 
+    Musician *a = O.get_members();
+
+    for(int i = 0; i<O.get_current_number_of_members();i++){
+        cout << a[i].get_instrument() << " " << a[i].get_experience() << endl;
+    }
+
+    cout << &a << endl;
     
 
 
