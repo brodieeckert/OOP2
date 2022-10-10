f8d1285f544410c9b0059e81cf3b8a1142fbc156
Brodie <brodieeckert29@gmail.com>
2022-10-11 08:38:58 +1030

    test

---
+++ USBConnection.h
@@ -0,0 +1,34 @@
+#include <stack>
+#include <iostream>
+using namespace std;
+class USBConnection
+{
+private:
+    int id;
+    static int count;
+
+    static int count;
+    USBConnection(){ id = USBConnection::count++;};
+
+public:
+
+    static USBConnection* CreateUsbConnection(){
+        if(count < 4){
+            USBConnection* a = new USBConnection();
+            return a;
+            
+
+        }
+        return nullptr;
+    }
+    int get_id(){
+        return this->id;
+    }
+    ~USBConnection();
+};
+int  USBConnection::count = 0;
+
+USBConnection::~USBConnection()
+{
+    USBConnection::count = this->id;
+}
