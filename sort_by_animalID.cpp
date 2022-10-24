6a4ebb453c42209aa25afc2159417bcfacb602e1
Brodie <brodieeckert29@gmail.com>
2022-10-25 10:27:10 +1030

    yeet

--- sort_by_animalID.cpp
+++ sort_by_animalID.cpp
@@ -9,9 +9,7 @@
 
 void sort_by_animalID::sort(animal **animals,int n){
     int i, j;
-    animal* buffer = nullptr;
     for (i = 0; i < n - 1; i++)
- 
         // Last i elements are already in place
         for (j = 0; j < n - i - 1; j++)
             if (animals[j]->get_animalID() > animals[j + 1]->get_animalID())
--- sort_by_name.cpp
+++ sort_by_name.cpp
@@ -9,13 +9,11 @@
 
 void sort_by_name::sort(animal **animals,int n){
     int i, j;
-    animal* buffer = nullptr;
     for (i = 0; i < n - 1; i++)
  
         // Last i elements are already in place
         for (j = 0; j < n - i - 1; j++)
-            if (animals[j]->get_name() < animals[j + 1]->get_name())
-                buffer = animals[j];
-                animals[j] = animals[j + 1];
-                animals[j+ 1 ] = buffer; buffer = nullptr;
+            if (animals[j]->get_name() > animals[j + 1]->get_name())
+                swap(animals[j],animals[j+1]);
+
 }	// sorts the array of n animals into ascending order using their animalIDs
