102b3fd2057b7c263a984029124cd2ccf5e21873
Brodie <brodieeckert29@gmail.com>
2022-10-26 16:27:31 +1030

    first try

--- Airplane.cpp
+++ Airplane.cpp
@@ -40,6 +40,6 @@ void Airplane::fly(int headwind, int minutes){
         this->numberOfFlights++;
 
     }
-    else{ this->fuel = 100}
+    else{ this->fuel = 100;}
     
 }         // headwind in km/h and minutes (time flying)
